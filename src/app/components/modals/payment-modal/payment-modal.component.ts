6aef8455c2629bd61863149c7ef54e814892e29b
Daniel <dhann9linux@googlemail.com>
2022-08-25 20:10:52 +0100

    added formgroup controls to modal

--- src/app/components/modals/payment-modal/payment-modal.component.ts
+++ src/app/components/modals/payment-modal/payment-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-modal',
@@ -17,22 +17,37 @@ export class PaymentModalComponent implements OnInit {
 
   // should really be a form group
 
-  card_number = new FormControl('', [
-    Validators.required,
-    Validators.maxLength(16),
-    Validators.minLength(16),
-  ]);
+  cardDetails = new FormGroup({
+    card_name: new FormControl('', [Validators.required]),
+    card_number: new FormControl('', [Validators.required, Validators.maxLength(16), Validators.minLength(16),]),
+    month_year: new FormControl('', [Validators.required]),
+    cvc: new FormControl('', [Validators.required, Validators.maxLength(3), Validators.minLength(3),])
+  });
 
-  month_year = new FormControl('', [Validators.required]);
+  getErrorMessage(message: String) {
+    if (message.valueOf() == "card_name") {
+      return this.cardDetails.get('card_name')?.hasError('required') ? 'You must enter a value' : '';
+    }
 
-  cvc = new FormControl('', [
-    Validators.required,
-    Validators.maxLength(3),
-    Validators.minLength(3),
-  ]);
+    else if (message.valueOf() == "card_number") {
+      return this.cardDetails.get('card_number')?.hasError('required') ? 'You must enter a value' :
+        this.cardDetails.get('card_number')?.hasError('minlength') || this.cardDetails.get('card_number')?.hasError('maxlength') ? 'must be a sixteen digit number' : '';
+    }
 
-  getErrorMessage() {
-    return this.card_number.hasError('required') ? 'You must enter a value' : '';
+    else if (message.valueOf() == "cvc") {
+      return this.cardDetails.get('cvc')?.hasError('required') ? 'You must enter a value' :
+        this.cardDetails.get('cvc')?.hasError('minlength') || this.cardDetails.get('cvc')?.hasError('maxlength') ? 'must be a three digit number' : '';
+
+    }
+
+    else if (message.valueOf() == "month_year") {
+      return 'month year'
+    }
+
+    else { return '' }
+    // console.log(this.cardDetails.getError)
+    // return this.cardDetails.hasError('required') ? 'You must enter a value' : '';
+
   }
 
   dismiss() {
